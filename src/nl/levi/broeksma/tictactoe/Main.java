6c338c36dcd52401d005015b606acbe1737d81d7
levibroeksma <levi@broeksma.nl>
2021-03-15 20:39:00 +0100

    First commit

---
+++ src/nl/levi/broeksma/tictactoe/Main.java
@@ -0,0 +1,21 @@
+package nl.levi.broeksma.tictactoe;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Tic Tac Toe");
+
+        //Stap 1: Maak een spelbord
+
+        //Stap 2: Laat speler 1 een X zetten
+
+        //Stap 3: Kijk of de speler heeft gewonnen
+
+        //Stap 4: Laat speler 2 een O zetten
+
+        //Stap 5: Kijk of de speler heeft gewonnen
+
+        //Herhaal stap 2 t/m 5 tot er een winnaar bekend is.
+    }
+}
